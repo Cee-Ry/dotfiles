dae25b0472bc9eab3344aaf56a0f3ef519690162
Cee-Ry <eduaveceasar@gmail.com>
2026-04-18 22:29:13 +0800

    set nvim line number color

--- .config/nvim/lua/user/theme.lua
+++ .config/nvim/lua/user/theme.lua
@@ -13,8 +13,8 @@ vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NonText", { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#00008B", bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFD700", bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "VertSplit", { bg = "NONE", ctermbg = "NONE" })
 
 -- Ensure syntax highlighting groups are visible
